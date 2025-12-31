dc031c76058f6b99df5060222ee230903248a457
Michelle Cordeiro <mic321@gmail.com>
2025-12-31 00:25:47 -0300

    feat: add Icon component with vite-plugin-svgr

--- 2-todo/src/App.tsx
+++ 2-todo/src/App.tsx
@@ -1,12 +1,34 @@
 import Text from './components/text';
+import Icon from './components/icon';
 
-
+import CheckIcon from './assets/icons/check.svg?react'
+import PlusIcon from './assets/icons/plus.svg?react'
+import SpinnerIcon from './assets/icons/spinner.svg?react'
+import PencilIcon from './assets/icons/pencil.svg?react'
+import XIcon from './assets/icons/x.svg?react'
+import TrashIcon from './assets/icons/trash.svg?react';
 
 export default function App() {
 
   return (
-    <Text variant={'body-md-bold'} className='text-pink-base '>
-      Olá mundo!
-    </Text>
-  )
+    <div className='grid gap-3'>
+      <div className='flex flex-col gap-2'>
+        <Text variant='body-sm-bold' className='text-pink-base'>
+          Olá mundo!
+        </Text>
+        <Text className='text-green-base'>Olá mundo!</Text>
+        <Text variant='body-md-bold'>Olá mundo!</Text>
+        <Text>Levar o dog pra passear</Text>
+      </div>
+
+      <div className='flex gap-1'>
+        <Icon svg={CheckIcon} className='fill-pink-base' />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate/>
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+        <Icon svg={TrashIcon} />
+      </div>
+    </div>
+  );
 }
